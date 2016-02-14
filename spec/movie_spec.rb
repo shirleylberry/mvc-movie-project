03db5a53911b43aee3e4895b8dd5006f70f8b943
shirleylberry <shirleylberry@gmail.com>
2016-02-14 17:22:46 -0500

    added additional tests

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -3,9 +3,17 @@ require 'spec_helper.rb'
 describe Movie do 
   describe '#initialize' do 
     let(:movie) { Movie.new("Space Jam") }
-
     it 'initializes the movie with a name' do 
       expect(movie.name).to eq("Space Jam")
+    end
+    it 'adds the movie to the @@all array' do 
+      expect(Movie.all).to include("Space Jam")
+    end
+  end
+  describe '.find_movie_by_name' do 
+    let(:movie) { Movie.new("Space Jam") }
+    it 'finds a movie by name' do 
+      expect(Movie.find_movie_by_name("Space Jam")).to eq(movie)
     end
   end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -11,4 +11,23 @@ describe User do
       expect(user1.movies).to eq([])
     end
   end
+  describe '#add_movie_by_name' do 
+    let(:user1) { User.new("Jeff") }
+    let(:movie1) { User.new("Space Jam") }
+    let(:movie2) { User.new("The Chipmunks Movie") }
+    it 'adds the movie to the user collection' do 
+      user1.add_movie_by_name("Space Jam")
+      user1.add_movie_by_name("The Chipmunks Movie")
+      expect(user1.movies).to eq(["Space Jam", "The Chipmunks Movie"])
+    end
+  end
+  describe '#delete_movie_by_name' do 
+    let(:user1) { User.new("Jeff") }
+    let(:movie1) { User.new("Space Jam") }
+    let(:movie2) { User.new("The Chipmunks Movie") }
+    it 'removes a movie from the user collection' do
+      user1.delete_movie_by_name("The Chipmunks Movie")
+      expect(user1.movies).not_to include("The Chipmunks Movie")
+    end
+  end
 end
