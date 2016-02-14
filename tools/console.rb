af00195a32e23d4a8e8468a524c9cf4f5f7637fa
shirleylberry <shirleylberry@gmail.com>
2016-02-14 17:23:13 -0500

    began creating CLI

--- tools/console.rb
+++ tools/console.rb
@@ -1,2 +1,21 @@
-require_relative '../environment.rb'
+require_relative '../config/environment.rb'
 
+puts "----------------------------"
+puts "-------- YOUR MOVIE --------"
+puts "-------- COLLECTION --------"
+puts "--------- MANAGER ----------"
+puts "----------------------------"
+puts "\n What action would you like to take?"
+puts "Your options are: add, lookup, update, and delete"
+action = gets.chomp
+
+case action
+  when 'add'
+  end
+  when 'lookup'
+  end
+  when 'update'
+  end
+  when 'delete'
+  end
+end
