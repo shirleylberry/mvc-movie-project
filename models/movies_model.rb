828636e601ebea6a749805148c3a40fcd0d3138e
shirleylberry <shirleylberry@gmail.com>
2016-02-14 18:14:16 -0500

    added methods to get movie data from omdb database

--- models/movies_model.rb
+++ models/movies_model.rb
@@ -1,7 +1,27 @@
+require "net/http"
+require "uri"
+require 'pry'
+
+# response = Net::HTTP.get_response("omdbapi.com","/?t=inception&format=json")
+
 class Movie
   attr_accessor :name
 
   def initialize(name)
     @name = name
+  end
+
+  def find_movie_by_name(movie_name)
+  end
+
+  def self.display_movie_data_by_name(movie_name)
+    response = Net::HTTP.get_response("omdbapi.com","/?t=#{movie_name}&format=json")
+    movie_info = eval(response.body)
+  end
+
+  def self.find_specific_movie_data_by_name(movie_name, movie_info)
+    response = Net::HTTP.get_response("omdbapi.com","/?t=#{movie_name}&format=json")
+    movie_info = eval(response.body)
+    movie_info[movie_info.capitaize.to_sym]
   end
 end
