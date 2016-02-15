f4a873174b810be17bb02509fcd968b871787657
chintas1 <chintas1@tcnj.edu>
2016-02-15 15:55:04 -0500

    Removes The Chipmunks Movie

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -26,12 +26,12 @@ describe Genre do
   describe '#delete_movie_by_name' do 
     let(:genre1) { Genre.new("Animation") }
     let(:movie1) { Movie.new("Frozen") }
-    let(:movie2) { Movie.new("The Chipmunks Movie") }
+    let(:movie2) { Movie.new("Finding Nemo") }
     it 'removes a movie from the genre list' do
       genre1.add_movie_by_name("Frozen")
-      genre1.add_movie_by_name("The Chipmunks Movie")
-      genre1.delete_movie_by_name("The Chipmunks Movie")
-      expect(genre1.movies).not_to include("The Chipmunks Movie")
+      genre1.add_movie_by_name("Finding Nemo")
+      genre1.delete_movie_by_name("Finding Nemo")
+      expect(genre1.movies).not_to include("Finding Nemo")
       expect(genre1.movies.size).to eq(1)
     end
   end
@@ -39,13 +39,13 @@ describe Genre do
     let(:genre1) { Genre.new("Animation") }
     let(:user1) {User.new("Ian")}
     let(:movie1) { Movie.new("Frozen") }
-    let(:movie2) { Movie.new("The Chipmunks Movie") }
+    let(:movie2) { Movie.new("Finding Nemo") }
     it 'calculates genre percentage for user given genre' do
       genre1.add_movie_by_name("Frozen")
-      genre1.add_movie_by_name("The Chipmunks Movie")
+      genre1.add_movie_by_name("Finding Nemo")
       user1.add_movie_by_name("Frozen")
       user1.add_movie_by_name("The Martian")
-      user1.add_movie_by_name("The Chipmunks Movie")
+      user1.add_movie_by_name("Finding Nemo")
       expect(genre1.get_genre_percentage(user1.movies)).to eq(67)
     end
   end
