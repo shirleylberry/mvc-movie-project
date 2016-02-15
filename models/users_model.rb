f0916eeb3427e7a33c1bc5e2b57a398aa54ed8e1
dgilbs <danielgilberg@gmail.com>
2016-02-15 13:32:50 -0500

    updates user model

--- models/users_model.rb
+++ models/users_model.rb
@@ -1,5 +1,8 @@
+require 'pry'
+
 class User
-  attr_accessor :name, :movies
+  attr_accessor :name, :movies, :favorite_movie, :email
+
 
   def initialize(name)
     @name = name
@@ -7,10 +10,42 @@ class User
   end
 
   def add_movie_by_name(movie)
-    @movies << "#{movie}" #if movie is in movies.all
+    @movies << movie #if movie is in movies.all
   end
 
   def delete_movie_by_name(movie)
     @movies.delete(movie)
   end
+
+  def favorite_movie=(movie)
+    @favorite_movie= @movies.find do |movie_name|
+                        movie_name==movie
+                    end
+  end
+
+  def last_movie_added
+    @movies.last
+  end
+
+  def movie_count
+    @movies.count 
+  end
+
+  def sort_collection
+    @movies.sort 
+  end
+
+
+  danny=User.new("Danny")
+  danny.add_movie_by_name("American Beauty")
+  danny.add_movie_by_name("Silence of the Lambs")
+  danny.favorite_movie=("American Beauty")
+  danny.email="danielgilberg@gmail.com"
+
+
+  binding.pry
+
+  puts "hi"
+
+
 end
