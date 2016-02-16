3d1f0e2c9db7b3c340ce8cdc6f5a4299f1eaed38
chintas1 <chintas1@tcnj.edu>
2016-02-16 14:24:13 -0500

    rearranges console.rb

--- tools/console.rb
+++ tools/console.rb
@@ -20,34 +20,35 @@ while action != "exit"
   prof_or_coll = gets.chomp
   exit if prof_or_coll == 'exit'
   case prof_or_coll
-  when 'collection'
-    action = movie_controller.start
-    exit if action == 'exit'
-    movie_name = movie_controller.prompt_movie_name.capitalize
-    case action
-      when 'add'
-        user_controller.add(current_user, movie_name)
-      when 'lookup'
-        user_controller.add_if_not_added(current_user, movie_name) unless movie_controller.display(movie_name).nil?
-      when 'remove'
-        user_controller.remove(current_user, movie_name)
-    end
-  when 'profile'
-    action = user_controller.start
-    case action
-      when 'E'
-        exit
-      when 'C'
-        puts current_user.movies
-      when 'L'
-        movie_controller.display(current_user.last_movie_added)
-      when 'P'
-        #should be handeld by users controller#get_profile
-        user_controller.profile(current_user)
-      when 'G'
-        #should be handeled by users controller#favgenre
-        puts current_user.get_fav_genre
-    end
+    when 'collection'
+      action = movie_controller.start
+      exit if action == 'exit'
+      case action
+        when 'add'
+          movie_name = movie_controller.prompt_movie_name.capitalize
+          user_controller.add(current_user, movie_name)
+        when 'lookup'
+          user_controller.add_if_not_added(current_user, movie_name) unless movie_controller.display(movie_name).nil?
+        when 'remove'
+          movie_name = movie_controller.prompt_movie_name.capitalize
+          user_controller.remove(current_user, movie_name)
+      end
+    when 'profile'
+      action = user_controller.start
+      case action
+        when 'E'
+          exit
+        when 'C'
+          puts current_user.movies
+        when 'L'
+          movie_controller.display(current_user.last_movie_added)
+        when 'P'
+          #should be handeld by users controller#get_profile
+          user_controller.profile(current_user)
+        when 'G'
+          #should be handeled by users controller#favgenre
+          puts current_user.get_fav_genre
+      end
   end
   "---------------------------"
 end
