a1526cd9a325c8620cd943488c8d7c355aec6553
chintas1 <chintas1@tcnj.edu>
2016-02-17 10:12:34 -0500

    adds movie/display erb

--- controllers/movies_controller.rb
+++ controllers/movies_controller.rb
@@ -19,7 +19,19 @@ class MoviesController
     movie = Movie.add_or_find_movie_by_name(movie_name)
     # binding.pry
     return if movie.nil?
-    view = MovieDisplayView.new
-    view.render(movie_name, movie.movie_data) unless movie.nil?
+    @movie_data = movie.movie_data
+    @movie_name = movie_name
+    render("movie/display") unless movie.nil?
+  end
+
+  def render(file)
+    file_name = "views/templates/#{file}.html.erb"
+    file_contents = File.read(file_name)
+    template = ERB.new(file_contents)
+    formatted_file_contents = template.result(binding)
+    # binding.pry
+    output_file = "views/output/#{file}.html"
+    new_file = File.write(output_file, formatted_file_contents)
+    `open -a 'Google Chrome' #{output_file}`
   end
 end
